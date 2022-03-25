2dc0df936d1ce14fbad3c22701a51dbc96c2f1f2
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2022-03-25 18:29:59 -0300

    Básico feito

---
+++ src/main/java/controllers/Controller.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import application.RepositoryException;
+import application.Util;
+import model.DefaultEntity;
+import repository.Repository;
+
+public abstract class Controller <T extends DefaultEntity>{
+	private Repository<T> repository;
+	protected T entity;
+	
+	public Controller(Repository<T> repository) {
+		super();
+		this.repository = repository;
+	}
+
+	public void incluir() {
+		try {
+			getRepository().save(getEntity());
+			Util.addInfoMessage("Inclusão realizada com sucesso.");
+			limpar();
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+			Util.addErrorMessage(e.getMessage());
+		}
+	}
+
+	public void alterar() {
+		try {
+			getRepository().save(getEntity());
+			Util.addInfoMessage("Alteração realizada com sucesso.");
+			limpar();
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+			Util.addErrorMessage(e.getMessage());
+		}
+	}
+	
+	public void excluir() {
+		try {
+			getRepository().remove(getEntity());
+			Util.addInfoMessage("Exclusão realizada com sucesso.");
+			limpar();
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+			Util.addErrorMessage(e.getMessage());
+		}
+	}
+
+	
+	public void limpar() {
+		entity = null;
+	}
+	
+	public Repository<T> getRepository() {
+		return repository;
+	}
+	
+	public abstract T getEntity();
+	
+	public void setEntity(T entity) {
+		this.entity = entity;
+	}
+}
